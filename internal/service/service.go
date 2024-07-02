80f82404797a6284b6dae18b6122cb062c444d74
Ярослав Молодцов <105973491+goodYaroslav@users.noreply.github.com>
2024-07-02 21:59:02 +0300

    changed sign-in response

--- internal/service/service.go
+++ internal/service/service.go
@@ -13,7 +13,7 @@ type User interface {
 	GetById(id entity.ObjectID) (entity.User, error)
 	GetAll() ([]entity.User, error)
 
-	Authenticate(credentials entity.UserAuth) (entity.JWTToken, error)
+	Authenticate(credentials entity.UserAuth) (entity.AuthData, error)
 	GenerateToken(u entity.User) (entity.JWTToken, error)
 	ParseToken(token entity.JWTToken) (entity.ObjectID, error)
 }
